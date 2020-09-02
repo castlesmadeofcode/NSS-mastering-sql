0facf621d96f2f8bbc33261b7bc6076412e44a14
castle <castlesmadeofcode@gmail.com>
2020-09-02 14:48:20 -0500

    fixed spacing

--- book-2/practice-exercises/chapter-1.sql
+++ book-2/practice-exercises/chapter-1.sql
@@ -1,8 +1,8 @@
 
 -- Practice: Dealers
+
 -- Write a query that returns the business name, city, state, and
 -- website for each dealership. Use an alias for the Dealerships table.
-
 SELECT
     d.business_name,
     d.city,
@@ -12,10 +12,9 @@ FROM Dealerships d
 
 
 -- Practice: Customers
+
 -- Write a query that returns the first name, last name, and 
 -- email address of every customer. Use an alias for the Customers table.
-
-
 SELECT
     c.first_name,
     c.last_name,
